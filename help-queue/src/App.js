1334b9b4fa7f499705287d029d78a2936fe18cb1
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-18 18:53:28 -0700

    store names and then populate them

--- help-queue/src/App.js
+++ help-queue/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 function App() {
+	const name = "Thato";
+	const name2 = "Haley";
 	return (
 		<React.Fragment>
 			<h1>Help Queue</h1>
 			<h3>3a</h3>
-			<h3>Thato and Haley</h3>
-			<p><em>Firebase entries not saving!</em></p>
-			<hr/>
+			<h3>
+				{name} and {name2}
+			</h3>
+			<p>
+				<em>Firebase entries not saving!</em>
+			</p>
+			{/* This is a JSX comment. */}
+			<hr />
 		</React.Fragment>
 	);
 }
