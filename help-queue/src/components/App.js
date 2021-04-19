38abb16a06e65bae3599026aa92e72a45ba25aaf
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-18 20:14:10 -0700

    import header and put it inside app

--- help-queue/src/components/App.js
+++ help-queue/src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Header from './Header';
 
 function App() {
 	const name = "Thato";
 	const name2 = "Haley";
 	return (
 		<React.Fragment>
-			<h1>Help Queue</h1>
+			<Header />
 			<h3>3a</h3>
 			<h3>
 				{name} and {name2}
