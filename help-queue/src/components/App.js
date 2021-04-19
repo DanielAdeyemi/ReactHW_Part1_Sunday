7d498a37537750d914a799e5c56e6a34e2bfefbd
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-18 20:26:12 -0700

    update app with ticketlist

--- help-queue/src/components/App.js
+++ help-queue/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import TicketList from './TicketList';
 
 function App() {
 	const name = "Thato";
@@ -7,15 +8,7 @@ function App() {
 	return (
 		<React.Fragment>
 			<Header />
-			<h3>3a</h3>
-			<h3>
-				{name} and {name2}
-			</h3>
-			<p>
-				<em>Firebase entries not saving!</em>
-			</p>
-			{/* This is a JSX comment. */}
-			<hr />
+			<TicketList />
 		</React.Fragment>
 	);
 }
