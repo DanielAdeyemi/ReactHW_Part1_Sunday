d058bfb7848e3d7fc6ab96baf1ef3ab74783f66b
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-18 20:38:02 -0700

    update ticket to use props

--- help-queue/src/components/Ticket.js
+++ help-queue/src/components/Ticket.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-function Ticket() {
-  const name = "Thato";
-  const name2 = "Haley";
+function Ticket(props) {
   return (
     <React.Fragment>
-      <h3>3a</h3>
+      <h3>{props.location} - {props.names}</h3>
       <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving</em></p>
+      <p><em>{props.issue}</em></p>
       <hr />
     </React.Fragment>
   );
