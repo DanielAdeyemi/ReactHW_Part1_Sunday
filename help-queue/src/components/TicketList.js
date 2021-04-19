69b96152edcd6e0365672c370b7938a6906d686a
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-18 22:06:26 -0700

    add map to loop through an array

--- help-queue/src/components/TicketList.js
+++ help-queue/src/components/TicketList.js
@@ -22,14 +22,13 @@ const masterTicketList = [
 function TicketList() {
   return (
     <React.Fragment>
-      <Ticket 
-        location="3A"
-        names="Thato and Haley"
-        issue="Firebase will not save record!"/>
-      <Ticket 
-        location="4B"
-        names="Sleater and Kinney"
-        issue="Prop types are throwing an error."/>
+      <hr />
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+          location={ticket.location}
+          issue={ticket.issue}
+          key={index} />
+      )}
     </React.Fragment>
   );
 }
