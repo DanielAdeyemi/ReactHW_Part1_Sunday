e749f28796bc1e08b1a7d077086d822f99e6dc0d
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-18 22:03:21 -0700

    add array of tickets

--- help-queue/src/components/TicketList.js
+++ help-queue/src/components/TicketList.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import Ticket from './Ticket';
 
+const masterTicketList = [
+	{
+		names: "Thato and Haley",
+		location: "3A",
+		issue: "Firebase won't save record. Halp.",
+	},
+	{
+		names: "Sleater and Kinney",
+		location: "4B",
+		issue: "Prop types are throwing an error.",
+	},
+	{
+		names: "Imani & Jacob",
+		location: "9F",
+		issue: "Child component isn't rendering.",
+	},
+];
+
 function TicketList() {
   return (
     <React.Fragment>
