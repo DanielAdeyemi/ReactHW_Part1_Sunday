8df909c030cc9a4ac1596e5c6e5155ac8121abc8
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-19 21:29:16 -0700

    add ticket button

--- help-queue/src/components/TicketControl.js
+++ help-queue/src/components/TicketControl.js
@@ -6,20 +6,23 @@ export default class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage = false
+      formVisibleOnPage : false
     };
   }
 
   render() {
     let currentlyVisibleState = null;
+    let addTicketButton = null;
     if(this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTicketForm />
     } else {
       currentlyVisibleState = <TicketList />
+      addTicketButton = <button onClick={this.handleClick}>Add Ticket </button>
     }
     return(
       <>
         {currentlyVisibleState}
+        {addTicketButton}
       </>
     );
   }
