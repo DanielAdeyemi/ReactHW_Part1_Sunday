73811c9a78f8fc35188985844e1586686fa5d18a
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-19 20:32:33 -0700

    creatr ticket control class

---
+++ help-queue/src/components/TicketControl.js
@@ -0,0 +1,17 @@
+import React from "react";
+import NewTicketForm from "./NewTicketForm";
+import TicketList from "./TicketList";
+
+export default class TicketControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <>
+      </>
+    );
+  }
+}
