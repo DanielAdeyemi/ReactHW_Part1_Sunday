4863af293395d30a7c380048ac5dd320f59a1fc9
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-20 20:45:57 -0700

    update ticket

--- help-queue/src/components/Ticket.js
+++ help-queue/src/components/Ticket.js
@@ -3,17 +3,25 @@ import PropTypes from 'prop-types';
 
 function Ticket(props) {
   return (
-    <React.Fragment>
-      <h3>{props.location} - {props.names}</h3>
-      <p><em>{props.issue}</em></p>
-      <hr />
-    </React.Fragment>
-  );
+		<React.Fragment>
+			<div onClick={() => props.whenTickedClicked(props.id)}>
+				<h3>
+					{props.location} - {props.names}
+				</h3>
+				<p>
+					<em>{props.issue}</em>
+				</p>
+				<hr />
+			</div>
+		</React.Fragment>
+	);
 }
 
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  issue: PropTypes.string
+  issue: PropTypes.string,
+  id: PropTypes.string,
+  whenTickedClicked: PropTypes.func
 };
 export default Ticket;
