86cf0bcf4150254b812153a50a5f4df2bbe72cc8
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-26 14:36:08 -0700

    add delete button to a ticket details

--- help-queue/src/components/TicketDetail.js
+++ help-queue/src/components/TicketDetail.js
@@ -2,17 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default function TicketDetail(props) {
-  const { ticket } = props;
+  const { ticket, onClickingDelete } = props;
   return(
     <>
       <h1>Ticlet Details</h1>
       <h3>{ticket.location} - {ticket.names}</h3>
       <p><em>{ticket.issue}</em></p>
+      <button onClick={() =>onClickingDelete(ticket.id)}>Close Ticket</button>
       <hr />
     </>
   )
 }
 
 TicketDetail.propTypes = {
-  ticket: PropTypes.object
+  ticket: PropTypes.object,
+  onClickingDelete: PropTypes.func
 };
