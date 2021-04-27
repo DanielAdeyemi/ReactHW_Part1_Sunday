33ba76bee8680fffac463e016e33bbbfd557cffd
DanielAdeyemi <adeyemidany@gmail.com>
2021-04-27 11:52:26 -0700

    add editing form

---
+++ help-queue/src/components/EditTicketForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+
+export default function EditTicketForm(props) {
+  return(
+    <>
+      <ReusableForm
+        buttonText="Update Ticket" />
+    </>
+  );
+}
