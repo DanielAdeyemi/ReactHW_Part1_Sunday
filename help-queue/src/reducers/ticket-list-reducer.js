5b697f902db9a87181055399fe42f894bc2d4fc9
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-02 22:32:23 -0700

    pass test 2 by updating reducer

--- help-queue/src/reducers/ticket-list-reducer.js
+++ help-queue/src/reducers/ticket-list-reducer.js
@@ -1,1 +1,16 @@
-export default (state = {}, action) => {return state};
+export default (state = {}, action) => {
+  const {names, location, issue, id} = action;
+  switch (action.type) {
+    case 'ADD_TICKET':
+      return Object.assign({}, state, {
+        [id]: {
+          names: names,
+          location: location,
+          issue: issue,
+          id: id
+        }
+      });
+      default: 
+        return state;
+  }
+};
