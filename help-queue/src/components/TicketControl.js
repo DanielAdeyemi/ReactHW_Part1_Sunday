f6e77bd1a0dbb25643794271ee76a25bf3720413
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-03 21:48:04 -0700

    add mapstateprops

--- help-queue/src/components/TicketControl.js
+++ help-queue/src/components/TicketControl.js
@@ -123,5 +123,10 @@ class TicketControl extends React.Component {
 	}
 }
 
-TicketControl = connect()(TicketControl);
+const mapStateToProps = state => {
+	return {
+		masterTicketList: state
+	}
+}
+TicketControl = connect(mapStateToProps)(TicketControl);
 export default TicketControl;
