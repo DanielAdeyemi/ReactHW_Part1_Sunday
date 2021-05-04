7455999a12edfd914d1ffd92591336171f7cfc08
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-03 21:05:59 -0700

    update index.js with reducer and store

--- help-queue/src/index.js
+++ help-queue/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import reducer from './reducers/ticket-list-reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
