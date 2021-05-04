71c038df7859ac687816bc476422731159d67196
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-04 14:40:06 -0700

    pass smole tests for reducers

--- help-queue/src/__tests__/reducers/index-reducer.test.js
+++ help-queue/src/__tests__/reducers/index-reducer.test.js
@@ -1,10 +1,22 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import ticketListReducer from '../../reducers/ticket-list-reducer';
 
+let store = createStore(rootReducer);
 describe("rootReducer", () =>{
   test('Should return default state if no action type recognized', () => {
     expect(rootReducer({}, {type: null })).toEqual({
       masterTicketList: {},
       formVisibleOnPage: false
     });
+  });
+
+  test('Check that initial state of ticketListReducer matches root reducer' , () => {
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, { type: null}));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, {type: null}));
   });
 });
