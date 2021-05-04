154796a6a7fbe9844e88df1428bf08c4adf560c6
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-04 13:49:09 -0700

    create form visible reducer test

---
+++ help-queue/src/__tests__/form-visible-reducer.test.js
@@ -0,0 +1,7 @@
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+
+describe("formVisibleReducer", () => {
+  TextDecoderStream('Should return default state if no action type is recognized', () => {
+    expect(formVisibleReducer(false, {type: null})).toEqual(false);
+  });
+});
