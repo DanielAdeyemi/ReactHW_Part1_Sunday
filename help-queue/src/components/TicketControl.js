244e9e12325480caeba469ff9cfc006804e19805
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-03 21:32:46 -0700

    update edit handler

--- help-queue/src/components/TicketControl.js
+++ help-queue/src/components/TicketControl.js
@@ -34,11 +34,17 @@ class TicketControl extends React.Component {
 	}
 
 	handleEditingTicketInList = (ticketToEdit) => {
-		const editedMasterTicketList = this.state.masterTicketList
-			.filter(ticket => ticket.id !== this.state.selectedTicket.id)
-			.concat(ticketToEdit);
+		const { dispatch } = this.props;
+		const { id, names, location, issue } = ticketToEdit;
+		const action = {
+			type: 'ADD_TICKET',
+			id: id,
+			names: names,
+			location: location,
+			issue: issue
+		}
+		dispatch(action);
 		this.setState({
-			masterTicketList: editedMasterTicketList,
 			editing: false,
 			selectedTicket: null
 		});
