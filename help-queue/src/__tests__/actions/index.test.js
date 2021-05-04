b7d6574049d03b88227b0e1b80db0b1f8b2907dd
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-04 15:31:20 -0700

    create test for toggle form action

--- help-queue/src/__tests__/actions/index.test.js
+++ help-queue/src/__tests__/actions/index.test.js
@@ -7,4 +7,10 @@ describe('help queue actions', () => {
       id: 1
     });
   });
+
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
 });
