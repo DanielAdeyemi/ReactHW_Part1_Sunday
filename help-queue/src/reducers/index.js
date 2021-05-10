79d136a0618a635a2f192085911a249430e291bf
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-10 15:13:53 -0700

    add firestore reducer

--- help-queue/src/reducers/index.js
+++ help-queue/src/reducers/index.js
@@ -1,10 +1,13 @@
 import formVisibleReducer from './form-visible-reducer';
 import ticketListReducer from './ticket-list-reducer';
 import { combineReducers } from 'redux';
+import { firestoreReducer } from "redux-firestore";
+import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
-  masterTicketList: ticketListReducer
+  masterTicketList: ticketListReducer,
+  firestore: firebaseReducer
 });
 
 export default rootReducer;
