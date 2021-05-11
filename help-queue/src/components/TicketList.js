923c96241de7a58d9b3babd3f89d4fe476c3190a
DanielAdeyemi <adeyemidany@gmail.com>
2021-05-10 21:07:00 -0700

    update ticket list with hooks

--- help-queue/src/components/TicketList.js
+++ help-queue/src/components/TicketList.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import Ticket from './Ticket';
+import React from "react";
+import Ticket from "./Ticket";
 import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
+import { useFirestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
 
 export default function TicketList(props) {
-  return (
-    <>
-      <hr />
-      {Object.values(props.ticketList).map((ticket) =>
-        <Ticket 
-					whenTickedClicked = {props.onTicketSelection}
-					names={ticket.names}
-          location={ticket.location}
-          issue={ticket.issue}
-					id={ticket.id}
-          key={ticket.id} />
-      )}
-    </>
-  );
+	useFirestoreConnect([{ collection: "tickets" }]);
+
+	const tickets = useSelector((state) => state.firestore.ordered.tickets);
+	if (isLoaded(tickets)) {
+		return (
+			<>
+				<hr />
+				{tickets.map((ticket) => (
+					<Ticket
+						whenTickedClicked={props.onTicketSelection}
+						names={ticket.names}
+						location={ticket.location}
+						issue={ticket.issue}
+						id={ticket.id}
+						key={ticket.id}
+					/>
+				))}
+			</>
+		);
+	} else {
+    return (
+      <>
+        <h3>Loading...</h3>
+      </>
+    )
+  }
 }
 
 TicketList.propTypes = {
-	ticketList : PropTypes.object,
-	onTicketSelection: PropTypes.func
-}
+	onTicketSelection: PropTypes.func,
+};
